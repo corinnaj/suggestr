66556ea9e27c0dc6a3340a516d787ddc844168c7
Corinna Jaschek <corinna.jaschek@gmail.com>
2020-08-05 16:01:02 +0200

    recipe planning

---
+++ lib/data.dart
@@ -0,0 +1,136 @@
+import 'package:flutter/material.dart';
+
+class Suggestion {
+  final String name;
+  final String pictureUrl;
+  final String description;
+
+  Suggestion(this.name, this.pictureUrl, this.description);
+
+  Image getImage(double width, double height) {
+    return Image.asset(
+      pictureUrl == '' ? 'images/image_missing.jpg' : 'images/' + pictureUrl,
+      fit: BoxFit.cover,
+      width: width,
+      height: height,
+    );
+  }
+}
+
+final List<Suggestion> suggestions = [
+  Suggestion(
+    'Aloo Paratha',
+    '',
+    "https://www.vegrecipesofindia.com/aloo-paratha-indian-bread-stuffed-with-potato-filling/",
+  ),
+  Suggestion(
+    'Lebanese Chickpeas',
+    'lebanese_chickpeas.jpg',
+    'TODO',
+  ),
+  Suggestion(
+    'Garlic Chicken',
+    '',
+    'https://www.halfbakedharvest.com/20-minute-basil-cashew-chicken/',
+  ),
+  Suggestion(
+    'Galette',
+    '',
+    'https://www.marmiton.org/recettes/recette_la-pate-a-galettes-de-ble-noir-traditionnelle_35351.aspx',
+  ),
+  Suggestion(
+    'Dosa & Aloo Masala',
+    '',
+    "https://cooking.nytimes.com/recipes/1020908-dosa?action=click&module=Global%20Search%20Recipe%20Card&pgType=search&rank=1",
+  ),
+  Suggestion(
+    'Pad Thai',
+    '',
+    'https://pinchofyum.com/rainbow-vegetarian-pad-thai-with-peanuts-and-basil',
+  ),
+  Suggestion(
+    'Pizza',
+    '',
+    '',
+  ),
+  Suggestion(
+    'Bolognese',
+    '',
+    '',
+  ),
+  Suggestion(
+    'Käsespätzle',
+    '',
+    '',
+  ),
+  Suggestion(
+    'Wild Rice and Mushroom Burger',
+    '',
+    'https://www.pickuplimes.com/single-post/2019/10/24/Wild-Rice-Mushroom-Burger',
+  ),
+  Suggestion(
+    'Banh Mi',
+    '',
+    'https://www.pickuplimes.com/single-post/2020/01/23/TOFU-VIETNAMESE-SUB-B%C3%81NH-M%C3%8C',
+  ),
+  Suggestion(
+    'Little Italy Burger',
+    '',
+    '',
+  ),
+  Suggestion(
+    'Kung Pao Cauliflower',
+    '',
+    '',
+  ),
+  Suggestion(
+    'Kidney Bean Burger',
+    '',
+    'Asien Vegetarisch - page ?',
+  ),
+  Suggestion(
+    'Butter Cauliflower',
+    '',
+    'https://tasty.co/recipe/easy-butter-chicken',
+  ),
+  Suggestion(
+    'Massaman Curry',
+    '',
+    'Asien Vegetarisch - page 114',
+  ),
+  Suggestion(
+    'Sommerpilaw',
+    '',
+    'Asien Vegetarisch - page 140',
+  ),
+  Suggestion(
+    'Rote Bete Reis',
+    '',
+    'Asien Vegetarisch - page 153',
+  ),
+  Suggestion(
+    'Singapore Fried Rice',
+    '',
+    'https://www.vegrecipesofindia.com/singapore-fried-rice-recipe/',
+  ),
+  Suggestion(
+    'Chilli',
+    '',
+    '',
+  ),
+  Suggestion(
+    'Aloo Gobi',
+    '',
+    'https://www.cookwithmanali.com/aloo-gobi/',
+  ),
+  Suggestion(
+    'Fennel Spaghetti',
+    '',
+    'https://www.bbcgoodfood.com/recipes/fennel-spaghetti',
+  ),
+  Suggestion(
+    'Lemon Tofu',
+    'lemon_tofu.jpg',
+    'Lemon Tofu: https://www.reddit.com/r/VegRecipes/comments/hk6lad/sticky_lemon_pepper_tofu/, goes well with rice and vegetables in a soy/oyster sauce with some lemon',
+  ),
+];
---
+++ lib/main.dart
@@ -0,0 +1,242 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:suggestr/data.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Suggestr(),
+    );
+  }
+}
+
+class Suggestr extends StatefulWidget {
+  @override
+  _SuggestrState createState() => _SuggestrState();
+}
+
+class _SuggestrState extends State<Suggestr> {
+  List<Suggestion> currentSuggestions;
+
+  @override
+  void initState() {
+    super.initState();
+    currentSuggestions = getNewSuggestions();
+  }
+
+  List<Suggestion> getNewSuggestions() {
+    suggestions.shuffle();
+    return suggestions.getRange(0, 12).toList();
+  }
+
+  void generateNewSuggestion(int index) {
+    setState(() {
+      suggestions.shuffle();
+      currentSuggestions[index] = suggestions[0];
+    });
+  }
+
+  String exportSuggestions() {
+    String result = '';
+    for (int i = 0; i < 7; i++) {
+      result += days[i];
+      result += ':\n';
+      result += currentSuggestions[i].name;
+      result += '\n';
+      result += currentSuggestions[i].description;
+      result += '\n\n';
+    }
+    return result;
+  }
+
+  static const List<String> days = ['Thu', 'Fri', 'Sat', 'Sun', 'Mon', 'Tue', 'Wed'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ...days.map((String day) => Day(day)),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 20.0),
+                  child: RaisedButton(
+                    color: Theme.of(context).primaryColor,
+                    textColor: Colors.white,
+                    onPressed: () {
+                      Clipboard.setData(ClipboardData(text: exportSuggestions()));
+                    },
+                    child: Icon(Icons.content_copy),
+                  ),
+                )
+              ],
+            ),
+          ),
+          //RaisedButton(
+          //child: Text('New Suggestions'),
+          //onPressed: () => setState(() => currentSuggestions = getNewSuggestions()),
+          //),
+          Suggestions(currentSuggestions, generateNewSuggestion),
+        ],
+      ),
+    );
+  }
+}
+
+class Day extends StatefulWidget {
+  final String day;
+  Day(this.day);
+
+  @override
+  _DayState createState() => _DayState();
+}
+
+class _DayState extends State<Day> {
+  Suggestion suggestion;
+  Suggestion hoveredSuggestion;
+
+  Widget image() {
+    if (hoveredSuggestion != null) return hoveredSuggestion.getImage(150, 200);
+    if (suggestion != null) return suggestion.getImage(150, 200);
+    return Container();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print(suggestion);
+    return DragTarget<Suggestion>(
+      onAccept: (s) => setState(() {
+        suggestion = s;
+        hoveredSuggestion = null;
+      }),
+      onWillAccept: (s) {
+        setState(() {
+          hoveredSuggestion = s;
+        });
+        return true;
+      },
+      onLeave: (s) => setState(() {
+        hoveredSuggestion = null;
+      }),
+      builder: (context, candidate, rejected) => Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 20.0),
+        child: Stack(children: <Widget>[
+          Container(
+            height: 150,
+            width: 200,
+            color: Colors.grey,
+            child: image(),
+          ),
+          if (suggestion != null)
+            Positioned(
+              child: Text(
+                suggestion.name,
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    shadows: [Shadow(color: Colors.black, blurRadius: 4), Shadow(color: Colors.black, blurRadius: 8)]),
+              ),
+              width: 200,
+              left: 8,
+              top: 4,
+            ),
+          Positioned(
+            child: Text(
+              widget.day,
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 40,
+                  shadows: [Shadow(color: Colors.black, blurRadius: 4), Shadow(color: Colors.black, blurRadius: 8)]),
+            ),
+            bottom: 4,
+            left: 8,
+          ),
+        ]),
+      ),
+    );
+  }
+}
+
+class Suggestions extends StatelessWidget {
+  final List<Suggestion> suggestions;
+  final Function generateNewSuggestion;
+  Suggestions(this.suggestions, this.generateNewSuggestion);
+
+  final double width = 400;
+  final double height = 250;
+
+  Widget buildChild(Suggestion suggestion, {bool dragging = false}) {
+    return Container(
+      width: width,
+      height: height,
+      child: Stack(children: <Widget>[
+        suggestion.getImage(width, height),
+        if (dragging)
+          Container(
+            color: Colors.black54,
+            width: width,
+            height: height,
+          ),
+        Positioned(
+          width: width,
+          bottom: 0,
+          left: 4,
+          child: Padding(
+            padding: const EdgeInsets.all(4.0),
+            child: Text(
+              suggestion.name,
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 30,
+                  shadows: [Shadow(color: Colors.black, blurRadius: 4), Shadow(color: Colors.black, blurRadius: 8)]),
+            ),
+          ),
+        ),
+      ]),
+    );
+  }
+
+  Widget buildSuggestion(int index, Suggestion suggestion, Function generateNewSuggestion) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Draggable<Suggestion>(
+        feedback: Opacity(
+          opacity: 0.5,
+          child: suggestion.getImage(200, 150),
+        ),
+        childWhenDragging: buildChild(suggestion, dragging: true),
+        data: suggestion,
+        child: buildChild(suggestion),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Wrap(
+          children: suggestions
+              .asMap()
+              .map((int index, Suggestion suggestion) =>
+                  MapEntry(index, buildSuggestion(index, suggestion, generateNewSuggestion)))
+              .values
+              .toList()),
+    );
+  }
+}
